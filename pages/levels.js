16dda88fd6a4275dcecf1d2e6a1ed802c4282d04
rendybjunior <rendy.b.junior@gmail.com>
2020-06-27 17:49:01 +0700

    dynamically list all levels

--- pages/levels.js
+++ pages/levels.js
@@ -1,18 +1,39 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import Layout from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedCasesData } from '../lib/cases';
 
-export default function Levels() {
+export async function getStaticProps() {
+	const allCasesData = getSortedCasesData();
+	return {
+		props: {
+			allCasesData
+		}
+	};
+}
+
+export default function Levels({ allCasesData }) {
 	return (
 		<Layout>
 			<Head>
 				<title>Levels</title>
 			</Head>
-			<h1>Game Levels</h1>
+			<ul className={utilStyles.list}>
+				{allCasesData.map(({ id, title }) => (
+					<li className={utilStyles.listItem} key={id}>
+						{title}
+						{/* <Link href="/cases/[id]" as={`/cases/${id}`}>
+							{title}
+						</Link> */}
+					</li>
+				))}
+			</ul>
+			{/* <h1>Game Levels</h1>
 			<h2>Level 1</h2>
 			<Link href="/case">
 				<a>Case 1 - Unbreak My Heart</a>
-			</Link>
+			</Link> */}
 		</Layout>
 	);
 }
