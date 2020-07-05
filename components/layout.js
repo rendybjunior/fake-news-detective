749a8a219924bb69cf3c85f1dfa688ebf5d181d7
rendybjunior <rendy.b.junior@gmail.com>
2020-07-05 07:03:05 +0700

    fix header

--- components/layout.js
+++ components/layout.js
@@ -14,18 +14,8 @@ export default function Layout({ children, home, levels, score, story }) {
 	<div className={styles.container}>
 		<Head>
 		  <link rel="icon" href="/favicon.ico" />
-		  <meta
-			name="description"
-			content="Learn how to build a personal website using Next.js"
-		  />
-		  <meta
-			property="og:image"
-			content={`https://og-image.now.sh/${encodeURI(
-			  siteTitle
-			)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-		  />
-		  <meta name="og:title" content={siteTitle} />
-		  <meta name="twitter:card" content="summary_large_image" />
+		  <meta name="description" content="Fake News Detective Game" />
+			<title>{siteTitle}</title>
 		</Head>
 		<header className={styles.header}>
 		  {home ? (
